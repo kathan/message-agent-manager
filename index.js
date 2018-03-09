b7e28c3a3633f7a28cba58736210d344ce3f889c
Darrel Kathan <Darrel.Kathan@grainger.com>
2018-03-09 14:55:57 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,207 @@
+/*
+message-agent-manager
+*/
+const EventEmitter = require('events').EventEmitter;
+const fs = require('fs');
+const os = require('os');
+const dns = require('dns');
+const path = require('path');
+const Url = require('url');
+const util = require('util');
+
+const Async = require('async');
+const formidable = require('formidable');
+const MessageAgentWorkflow = require('./workflow.js');
+const express = require('express');
+
+var MessageAgentManager = function(options, callback){
+  if ( !(this instanceof MessageAgentManager) ){
+    return new MessageAgentManager(options, callback);
+  }
+  var self = this;
+  var log = options.log || function(){
+    var d = new Date();
+    const args = Array.from(arguments);
+    args.unshift(self.constructor.name);
+    args.unshift(`${d}`);
+    
+    console.log.apply(this, args);
+  };
+  var error = options.error || function (){
+    const args = Array.from(arguments);
+    args.unshift('ERROR:');
+    var e = new Error();
+    args.push(e.stack);
+    log.apply(null, args);
+  };
+  var log_dir = options.log_dir || 'logs';
+  var wf_dir = options.wf_dir || 'workflows';
+  var ip = os.networkInterfaces().en0[1].address;
+  var hostname;
+  var config_file_name = options.config_file_name || 'fam-cfg.json';
+  var port = options.port || 8080;
+  var wf_url;// = Url.resolve(`http://${hostname}:${port}`, 'workflows');
+  var app;
+  var workflows = [];
+  EventEmitter.call(this);
+  function getHostname(cb){
+    dns.reverse(ip, (err, hostnames)=>{
+      //if(err){return cb(err);}
+      hostname = hostnames[0] || ip;
+      log('hostname', hostname);
+      wf_url = Url.resolve(`http://${hostname}:${port}`, 'workflows');
+      cb();
+    });
+  }
+  this.getWorkflows = function(){
+    return workflows;
+  };
+  
+  function loadConfig(cb){
+    fs.readFile(path.resolve(__dirname, config_file_name), (err, data)=>{
+      if(err){
+        if(err.code !== 'ENOENT'){
+          error(err);
+          return cb(err);
+        }
+      }
+      if(data){
+        var json = JSON.parse(data);
+        if(json.workflows){
+          createWorkflows(json.workflows, ()=>{
+            cb();
+          });
+        }
+        return;
+      }
+      cb();
+    });
+  }
+  
+  function storeConfig(cb){
+    fs.writeFileSync(path.resolve(__dirname, config_file_name), JSON.stringify({workflows:workflows}));
+  }
+  
+  function createWorkflows(wrkflws, cb){
+    Async.forEach(wrkflws, 
+      (workflow, next)=>{
+        self.createWorkflow(workflow, (err)=>{
+          if(err){return next(err);}
+          next();
+        });
+      },
+      (err)=>{
+        if(err){return cb(err);}
+        cb();
+      }
+    );
+  }
+  
+  this.getWorkflow = function(name){
+    return workflows.find((workflow)=>{
+      return workflow.name === name;
+    });
+  };
+  
+  this.createWorkflow = function(opts, cb){
+    var w = self.getWorkflow(opts.name);
+    //log('existing workflow', w)
+    if(!w){
+      log(`Creating workflow ${opts.name}`);
+      var wf = MessageAgentWorkflow(app, {
+        name: opts.name,
+        running: opts.running || false,
+        url: Url.resolve(wf_url, opts.name),
+        directory: path.resolve(wf_dir, opts.name),
+        agents: opts.agents || [],
+        log: opts.log || log,
+        error: opts.error || error
+      }, (err)=>{
+        cb(err);
+      });
+      workflows.push(wf);
+    }else{
+      cb(`A workflow named ${opts.name} already exists.`);
+    }
+  };
+  
+  function createServer(cb){
+    app = express();
+    
+    app.set("json spaces", 2);
+    app.use((req, res, next)=>{
+      if (req.method.toLowerCase() == 'post') {
+        var form = new formidable.IncomingForm();
+        //console.log('Server received post. Parsing files...');
+        form.parse(req, function(err, fields, files) {
+          if(err){return;}
+          req.body = fields;
+          //req.files = files;
+
+          for(var i in files){
+            files[i].data = fs.readFileSync(files[i].path);
+          }
+          req.files = files;
+          //console.log('Files parsed.');
+          next();
+        });
+        return;
+      }
+      next();
+    });
+    
+    app.get('/', (req, res, next)=>{
+      res.json({workflows:workflows});  
+    });
+    
+    app.get('/workflows', (req, res, next)=>{
+      res.json(workflows);  
+    });
+
+    app.post(`/workflows`, (req, res, next) => {
+      //log('POST workflow id', id);
+      if(req.body.workflow){
+        //var w = self.getWorkflow(req.body.workflow);
+        //log('existing workflow', w);
+        self.createWorkflow({name: req.body.workflow}, (err, wf)=>{
+          res.json(wf);
+          next();
+        });
+        return;
+      }else{
+        next();
+      }
+    });
+
+    /*app.get(`/${name}/:id`, function (req, res, next, id) {
+      self.getWorkflow(id)
+      next();
+    });*/
+    
+    app.listen(port, () => {
+      log(`listening on ${port}`);
+      cb();
+    });
+  }
+  
+  process.on('SIGINT', () => {
+    storeConfig();
+    log('Quitting.');
+    process.exit(0);
+  });
+
+  createServer((err)=>{
+    if(err){error(err); return callback(err);}
+    getHostname((err)=>{
+      if(err){error(err); return callback(err);}
+      loadConfig((err)=>{
+        if(err){error(err); return callback(err);}
+        log('running...');
+        callback();
+      });
+    });
+  });
+
+};
+util.inherits(MessageAgentManager, EventEmitter);
+module.exports = MessageAgentManager;
---
+++ workflow.js
@@ -0,0 +1,179 @@
+/*
+message-agent-workflow
+*/
+const EventEmitter = require('events').EventEmitter;
+const path = require('path');
+const Url = require('url');
+const util = require('util');
+
+const Async = require('async');
+const MessageAgent = require('message-agent');
+
+var MessageAgentWorkflow = function(app, options, callback){
+  if ( !(this instanceof MessageAgentWorkflow) ){
+    return new MessageAgentWorkflow(app, options, callback);
+  }
+  var self = this;
+  //var running = options.running || false;
+  var agents = [];
+  var agent_opts = options.agents || [];
+  var log = options.log || function(){
+    var d = new Date();
+    const args = Array.from(arguments);
+    args.unshift(self.constructor.name);
+    args.unshift(`${d}`);
+    
+    console.log.apply(this, args);
+  };
+  var error = options.error || function (){
+    const args = Array.from(arguments);
+    args.unshift('ERROR:');
+    var e = new Error();
+    args.push(e.stack);
+    log.apply(null, args);
+  };
+  //log('agent_opts', agent_opts);
+  var name = options.name;//required
+  //==== Getters ====
+  Object.assign(this,
+    {
+      get name() {
+        return options.name;
+      }
+    }
+  );
+  var directory = options.directory; //required
+  var url = options.url;
+  var url_obj = Url.parse(url);
+  //log('url_obj', url_obj);
+  //options.running ? running = options.running : null;
+  EventEmitter.call(this);
+  this.toJSON = function (){
+    return {
+      name:name,
+      url: url,
+      directory:directory,
+      //running:running,
+      agents:agents
+    };
+  };
+  
+  /*this.stop = function(){
+    if(running){
+      running = false;
+      agents.forEach((agent)=>{
+        agent.stop();
+      });
+    }
+  };
+  
+  this.start = function(){
+    if(!running){
+      running = true;
+      agents.forEach((agent)=>{
+        agent.start();
+      });
+    }
+  };*/
+  
+  this.getAgent = function(name){
+    return agents.find((agent)=>{
+      return agent.name === name;
+    });
+  };
+  
+  this.createAgent = function(opts, cb){
+    var a = self.getAgent(opts.name);
+    //log('existing agent', a);
+    if(!a){
+      log(`Creating agent ${name}/${opts.name}`);
+      var fa = MessageAgent(app, {
+                          directory: path.resolve(directory, opts.name),
+                          name: opts.name,
+                          //running: opts.running,
+                          url: Url.resolve(url, `${name}/${opts.name}`),
+                          dest: opts.dest || null
+                          //log: log,
+                          //error: error
+                          }, (err)=>{
+        if(err){return cb(err);}
+        
+        //log(`Running agent ${opts.agent_name}`);
+        //fa.start();
+        cb();
+      });
+      if(agent_opts.script && typeof agent_opts.script === 'function'){
+        fa.on('file', agent_opts.script);
+      }
+      fa.on('error', (err)=>{
+        error('Error', err);
+      });
+      
+      agents.push(fa);
+      return fa;
+    }else{
+      cb(`An agent named ${opts.name} already exists.`);
+    }
+  };
+  
+  function createAgents(cb){
+    Async.forEach(agent_opts, 
+      (agent, next)=>{
+        self.createAgent(agent, (err)=>{
+          if(err){return next(err);}
+          next();
+        });
+      },
+      (err)=>{
+        if(err){return cb(err);}
+        cb();
+      }
+    );
+  }
+  
+  app.get(`${url_obj.pathname}`, (req, res, next)=>{
+    res.json(self);
+    next();
+  });
+  
+  /*app.put(`${url_obj.pathname}/start`, (req, res, next)=>{
+    self.start();
+    res.json({message:`Started ${name}`});
+    next();
+  });
+    
+  app.put(`${url_obj.pathname}/stop`, (req, res, next)=>{
+    self.stop();
+    res.json({message:`Stopped ${name}`});
+    next();
+  });*/
+  
+  app.post(`${url_obj.pathname}/agents`, (req, res, next)=>{
+    if(req.body.agent){
+      self.createAgent({
+        name: req.body.agent
+        //running: false
+        }, (err, wf)=>{
+        if(err){error(err);return res.json({error:err});}
+        res.json(wf);
+        next();
+      });
+      return;
+    }else{
+      next();
+    }
+    next();
+  });
+
+  createAgents((err)=>{
+    if(err){error(err); return callback(err);}
+    if(typeof callback === 'function'){
+      setImmediate(callback);
+    }else{
+      self.emit('ready');
+    }
+  });
+};
+
+util.inherits(MessageAgentWorkflow, EventEmitter);
+module.exports = MessageAgentWorkflow;
